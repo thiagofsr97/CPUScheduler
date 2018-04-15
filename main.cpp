dc5dd0078e459d380a49ab5d30472753566c5305
Thiago <thiago.filipe@lavid.ufpb.br>
2018-04-15 12:11:24 -0300

    Update makefile

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 #define BadArgs() std::cout << "Wrong or missing arguments." << std::endl;exit(1);
 
 int main(int argc,char**argv) {
-
+    std::cout << "-----------------------------Running Scheduling Algorithms-----------------------------\n\n";
     FileUtil fileUtils("in.txt","out.txt");
     std::vector<Process> processList;
+
     Scheduler scheduler(&fileUtils);
     scheduler.schedule(FCFS);
     fileUtils.appendToLog(FCFS,scheduler.getReturnAverage(),scheduler.getResponseAverage(),scheduler.getWaitAverage());
@@ -14,6 +15,8 @@ int main(int argc,char**argv) {
     fileUtils.appendToLog(SJF,scheduler.getReturnAverage(),scheduler.getResponseAverage(),scheduler.getWaitAverage());
     scheduler.schedule(RR);
     fileUtils.appendToLog(RR,scheduler.getReturnAverage(),scheduler.getResponseAverage(),scheduler.getWaitAverage());
+
+    std::cout << "--------------------------------Check the out.txt file.--------------------------------"<< std::endl;
 
     return 0;
 }
--- makefile
+++
@@ -1,9 +0,0 @@
-CC=g++
-CFLAGS=-I -std=c++11.
-DEPS = FileUtil.h Process.h Scheduler.h Util.h
-OBJ = main.o FileUtil.o Process.o Scheduler.o  
-%.o: %.cpp $(DEPS)
-	$(CC) -c -o $@ $< $(CFLAGS)
-
-CPUScheduler: $(OBJ)
-	g++ -o $@ $< $(CFLAGS)
